f1474a2c90d03d23f1010aadca1e8fa7083d1fa9
Ofir Azoulay-Rozanes <ofir@anjuna.io>
2021-10-31 17:28:03 -0700

    Added logging.

--- gCalNotifier.py
+++ gCalNotifier.py
@@ -16,6 +16,9 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 import validators
 
+import logging
+from logging.handlers import RotatingFileHandler
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
@@ -80,15 +83,59 @@ class Window(QMainWindow, Ui_w_event):
         if (p_button in self.snooze_buttons):
             snooze_time_in_minutes = self.snooze_buttons[p_button]
 
-            print(snooze_time_in_minutes)
+            logger.debug("Snooze time in minuetes " + str(snooze_time_in_minutes))
 
         else:
-            print("Snooze button not found", p_button)
+            logger.error("Snooze button not found " + str(p_button))
     
         self.close()
 
+def init_logging(module_name):
+    global logger
+
+    # create logger
+    logger = logging.getLogger(module_name)
+    logger.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # create console handler
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+
+    # add formatter to ch
+    console_handler.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(console_handler)
+
+    # Create file handler
+    log_file = module_name + ".log"
+    max_log_file_size = 100 * 1024 * 1024
+    file_handler = RotatingFileHandler(
+        log_file,
+        mode='a',
+        maxBytes=max_log_file_size,
+        backupCount=5,
+        encoding='utf-8')
+
+    file_handler.setLevel(logging.INFO)
+
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+
+    logger.info("========")
+    logger.info("Starting")
+    logger.info("========")
+
+    return logger
+
 # Identify the video meeting softwate via its URL
 def identify_video_meeting(win_label, url, text_if_not_identified):
+    global logger
+
     if ("zoom.us" in url):
         label_text = "Zoom Link"
     elif ("webex.com" in url):
@@ -105,6 +152,8 @@ def identify_video_meeting(win_label, url, text_if_not_identified):
     win_label.setToolTip(url)
 
 def get_events_from_google_cal(google_account):
+    global logger
+
     # Connect to the Google Account
     creds = None
     token_file = google_account + '/token.json'
@@ -130,7 +179,7 @@ def get_events_from_google_cal(google_account):
 
     # Call the Calendar API
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print('Getting the upcoming 10 events')
+    logger.debug('Getting the upcoming 10 events')
     events_result = service.events().list(calendarId='primary', timeMin=now,
                                         maxResults=10, singleEvents=True,
                                         orderBy='startTime').execute()
@@ -146,6 +195,7 @@ def show_window_and_parse_exit_status(event_id, parsed_event):
     global dismissed_events
     global snoozed_events
     global snooze_time_in_minutes
+    global logger
 
     win = Window()
 
@@ -161,7 +211,7 @@ def show_window_and_parse_exit_status(event_id, parsed_event):
     win.l_event_start.setText('Starting on ' + str(parsed_event['start_date']))
     win.l_event_end.setText('Ending on ' + str(parsed_event['end_date']))
 
-    #print(html_link)
+    logger.debug("HTML Link " + parsed_event['html_link'])
     win.l_event_link.setText("<a href=\"" + parsed_event['html_link'] + "\">Link to event in GCal</a>")
     win.l_event_link.setToolTip(parsed_event['html_link'])
 
@@ -196,7 +246,7 @@ def show_window_and_parse_exit_status(event_id, parsed_event):
     else:
         time_to_event_in_minutes = -1
     
-    #print(time_to_event_in_minutes)
+    logger.debug("Time to event in minutes " + str(time_to_event_in_minutes))
     for pb_button, snooze_time in win.snooze_buttons.items():
         if (snooze_time <= 0 and abs(snooze_time) > time_to_event_in_minutes):
             pb_button.setHidden(True)
@@ -211,12 +261,12 @@ def show_window_and_parse_exit_status(event_id, parsed_event):
 
     # Look at the window exit reason
     if (win_exit_reason == EXIT_REASON_NONE):
-        print("Cancel")
+        logger.debug("Cancel")
     elif (win_exit_reason == EXIT_REASON_DISMISS):
-        #print("Dismiss")
+        logger.debug("Dismiss")
         dismissed_events[event_id] = parsed_event['end_date']
     elif (win_exit_reason == EXIT_REASON_SNOOZE):
-        #print("Snooze")
+        logger.debug("Snooze")
         if (snooze_time_in_minutes <= 0):
             delta_diff = datetime.timedelta(minutes=abs(snooze_time_in_minutes))
             parsed_event['event_wakeup_time'] = parsed_event['start_date'] - delta_diff
@@ -224,43 +274,46 @@ def show_window_and_parse_exit_status(event_id, parsed_event):
             delta_diff = datetime.timedelta(minutes=snooze_time_in_minutes)
             parsed_event['event_wakeup_time'] = now_datetime + delta_diff
 
-        #print("Snooze until", parsed_event['event_wakeup_time'])
+        logger.debug("Snooze until " + str(parsed_event['event_wakeup_time']))
             
         snoozed_events[event_id] = parsed_event
     else:
-        print("No exit reason")
+        logger.error("No exit reason")
 
 def clear_dismissed_and_snoozed_events():
     global dismissed_events
     global snoozed_events
+    global logger
 
     now_datetime = get_now_datetime()
 
     # Clear dismissed events that have ended
     dismissed_events_to_delete = []
     for k, v in dismissed_events.items():
-        print(k, v, now_datetime)
+        logger.debug("Dismissed event " + str(k) + " " + str(v) + " " + str(now_datetime))
         if (now_datetime > v):
             dismissed_events_to_delete.append(k)
 
     while (len(dismissed_events_to_delete) > 0):
         k = dismissed_events_to_delete.pop()
-        print("Deleteing event id", k, "from dismissed")
+        logger.debug("Deleteing event id " + str(k) + " from dismissed")
         del dismissed_events[k]
 
     # Clear snoozed events that have ended
     snoozed_events_to_delete = []
     for k, v in snoozed_events.items():
-        #print(k, v['end_date'], now_datetime)
+        logger.debug("Snoozed event " + str(k) + " " + str(v['end_date']) + " " + str(now_datetime))
         if (now_datetime > v['end_date']):
             snoozed_events_to_delete.append(k)
 
     while (len(snoozed_events_to_delete) > 0):
         k = snoozed_events_to_delete.pop()
-        #print("Deleteing event id", k, "from snoozed")
+        logger.debug("Deleteing event id " + str(k) + " from snoozed")
         del snoozed_events[k]
 
 def parse_event(event, parsed_event):
+    global logger
+
     if (event['reminders'].get('useDefault') == True):
         minutes_before = 15
     else:
@@ -269,7 +322,7 @@ def parse_event(event, parsed_event):
             override_set = override_rule[0]
             minutes_before = override_set['minutes']
         else:
-            #print("No need to remind")
+            logger.debug("No need to remind")
             return(False)
 
     # Event needs to be reminded, check if it is the time to remind
@@ -319,29 +372,32 @@ def parse_event(event, parsed_event):
 def notify_on_needed_calendar_events(google_account):
     global dismissed_events
     global snoozed_events
+    global logger
+
+    logger.debug("notify_on_needed_calendar_events for " + google_account)
 
     # Get the next coming events from the google calendar
     events = get_events_from_google_cal(google_account)
 
     # Handled the snoozed events
     if not events:
-        print('No upcoming events found for', google_account)
+        logger.debug('No upcoming events found')
 
     for event in events:
-        #print(event)
+        logger.debug(event)
         parsed_event = {}
         now_datetime = get_now_datetime()
         a_snoozed_event_to_wakeup = False
 
         event_id = event['id']
-        #print(event_id)
+        logger.debug("Event ID " + str(event_id))
 
         parsed_event['event_name'] = event['summary']
         parsed_event['google_account'] = google_account
-        #print(parsed_event['event_name'])
+        logger.debug("Event Name " + parsed_event['event_name'])
 
         if (event_id in dismissed_events):
-            #print("Skipping dismissed event")
+            logger.debug("Skipping dismissed event")
             continue
 
         if (event_id in snoozed_events):
@@ -350,11 +406,11 @@ def notify_on_needed_calendar_events(google_account):
 
             if (snoozed_event['event_wakeup_time'] > now_datetime):
                 # The time of the snoozed event has not arrived yet
-                #print("Skipping snoozed event that should not be woke up yet")
+                logger.debug("Skipping snoozed event that should not be woke up yet")
                 continue
             else:
                 # Its time to wake up the snoozed even
-                #print("Time to wake up the snoozed event")
+                logger.debug("Time to wake up the snoozed event")
                 a_snoozed_event_to_wakeup = True
                 parsed_event = snoozed_event
 
@@ -378,10 +434,13 @@ if __name__ == "__main__":
     dismissed_events = {}
     snoozed_events = {}
 
+    # Init the logger
+    logger = init_logging("gCalNotifier")    
+
     # Loop forever
     while True:
         notify_on_needed_calendar_events('ofir_anjuna_io')
         notify_on_needed_calendar_events('ofiraz_gmail_com')
 
-        print("Going to sleep for 30 seconds")
+        logger.debug("Going to sleep for 30 seconds")
         time.sleep(30)
