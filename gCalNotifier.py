d6c9271e5783fbd93f74e65b9aeaca4c31a8d016
Ofir Azoulay-Rozanes <ofiraz@gmail.com>
2022-05-12 07:00:55 -0700

    Added support for Webex links in the description, and improved the generic support for video links in the description.

--- gCalNotifier.py
+++ gCalNotifier.py
@@ -692,6 +692,50 @@ def show_window_and_parse_exit_status(event_key_str, parsed_event):
     with g_displayed_lock:
         del g_displayed_events[event_key_str]
 
+video_links_reg_exs = [
+    "(https://[a-zA-Z0-9-]*\.zoom\.us/[a-zA-Z0-9-\.&?=/]*)", # Zoom
+    "Click here to join the meeting<(https://teams.microsoft.com/l/meetup-join/.*)>", # Meet   
+    "[<>](https://[a-zA-Z0-9-]*\.webex\.com/[a-zA-Z0-9-]*/j\.php\?MTID=[a-zA-Z0-9-]*)[<>]" # Webex
+]
+
+def look_for_video_link_in_meeting_description(p_meeting_description):
+    for reg_ex in video_links_reg_exs:
+        video_url_in_description = re.search(
+            reg_ex,
+            p_meeting_description)
+
+        if video_url_in_description:
+            return(video_url_in_description.group(1))
+
+    # No known video link found
+    return("No Video")
+
+    # Look for a Zoom link
+    zoom_url_in_description = re.search(
+        "(https://[a-zA-Z0-9-]*\.zoom\.us/[a-zA-Z0-9-\.&?=/]*)", 
+        p_meeting_description) 
+    if zoom_url_in_description:
+        return(zoom_url_in_description.group())
+
+    # Look for a Meet link
+    teams_url_in_description = re.search(
+        "Click here to join the meeting<(https://teams.microsoft.com/l/meetup-join/.*)>",
+        p_meeting_description) 
+
+    if teams_url_in_description:
+        return(teams_url_in_description.group(1))
+
+    # Look for a Webex link
+    webex_url_in_description = re.search(
+        ">(https://[a-zA-Z0-9-]*\.webex\.com/[a-zA-Z0-9-]*/j\.php\?MTID=[a-zA-Z0-9-]*)<",
+        p_meeting_description)
+
+    if webex_url_in_description:
+        return(webex_url_in_description.group(1))
+
+    # No known video link found
+    return("No Video")
+
 def parse_event(event, parsed_event):
     global g_logger
 
@@ -762,25 +806,10 @@ def parse_event(event, parsed_event):
         # Didn't find a video link in the expected location, let's see if there is a video link in the 
         # description.
         if (meeting_description):
-            zoom_url_in_description = re.search(
-                "(https://[a-zA-Z0-9-]*\.zoom\.us/[a-zA-Z0-9-\.&?=/]*)", 
-                meeting_description) 
-            #print(zoom_url_in_description.group())
-            if zoom_url_in_description:
-                parsed_event['video_link'] = zoom_url_in_description.group()
-            else:
-                # Check if there is a Microsoft Teams link in the description
-                teams_url_in_description = re.search(
-                    "Click here to join the meeting<(https://teams.microsoft.com/l/meetup-join/.*)>",
-                    meeting_description) 
-
-                #print(teams_url_in_description.group(1))
-
-                if teams_url_in_description:
-                    parsed_event['video_link'] = teams_url_in_description.group(1)
+            parsed_event['video_link'] = look_for_video_link_in_meeting_description(meeting_description)
 
             if (parsed_event['video_link'] == parsed_event['event_location']):
-                # The event location already contains the Zoom link, no need to show it twice
+                # The event location already contains the video link, no need to show it twice
                 parsed_event['video_link'] = "No Video"
 
     # The event needs to be notified
