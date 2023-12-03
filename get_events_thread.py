89b1347996c00eba0cd45460b8d1957fd63613fa
Ofir Azoulay-Rozanes <ofiraz@gmail.com>
2023-12-03 13:04:51 -0800

    Removing snoozed and dismissed events to the global objects in prep for the ability to display these events on demand

--- get_events_thread.py
+++ get_events_thread.py
@@ -24,13 +24,10 @@ from event_utils import (
 )
 
 def is_event_already_in_a_collection(globals, event_key_str):
-    global dismissed_events
-    global snoozed_events
-
     for events_collection_to_check in [
-        dismissed_events,
+        globals.dismissed_events,
         globals.events_to_dismiss,
-        snoozed_events,
+        globals.snoozed_events,
         globals.events_to_snooze,
         globals.displayed_events,
         globals.events_to_present]:
@@ -41,9 +38,6 @@ def is_event_already_in_a_collection(globals, event_key_str):
     return(False)
 
 def add_items_to_show_from_calendar(globals, google_account, cal_name, cal_id):
-    global dismissed_events
-    global snoozed_events
-
     globals.logger.debug("add_items_to_show_from_calendar for " + google_account)
 
     # Get the next coming events from the google calendar
@@ -101,11 +95,11 @@ def add_items_to_show_from_calendar(globals, google_account, cal_name, cal_id):
                 + " " + parsed_event['raw_event']['id'])
         elif (event_action == ACTION_DISMISS_EVENT):
             # No need to present the event - add it to the dismissed events
-            events_collection_to_add_the_event_to = dismissed_events
+            events_collection_to_add_the_event_to = globals.dismissed_events
         
         elif(event_action == ACTION_SNOOOZE_EVENT):
             # Too early to present the event
-            events_collection_to_add_the_event_to = snoozed_events
+            events_collection_to_add_the_event_to = globals.snoozed_events
 
         else:
             # Unexpected type
@@ -136,9 +130,7 @@ def condition_function_for_removing_snoozed_events(logger, event_key_str, parsed
     return(True)
 
 def set_items_to_present_from_snoozed(globals):
-    global snoozed_events
-
-    snoozed_events.remove_events_based_on_condition(
+    globals.snoozed_events.remove_events_based_on_condition(
         condition_function_for_removing_snoozed_events, 
         additional_param = globals.events_to_present)
 
@@ -166,27 +158,21 @@ def condition_function_for_removing_dismissed_events(logger, event_key_str, pars
     return(True)
 
 def move_events_to_dismiss_into_dismissed_events_collection(globals):
-    global dismissed_events
-
     while (True):
-        event_key_str, parsed_event = dismissed_events.pop_from_another_collection_and_add_this_one(globals.events_to_dismiss)
+        event_key_str, parsed_event = globals.dismissed_events.pop_from_another_collection_and_add_this_one(globals.events_to_dismiss)
         if (event_key_str == None):
             # No more entries to present
             return
 
 def move_events_to_snooze_into_snoozed_events_collection(globals):
-    global snoozed_events
-
     while (True):
-        event_key_str, parsed_event = snoozed_events.pop_from_another_collection_and_add_this_one(globals.events_to_snooze)
+        event_key_str, parsed_event = globals.snoozed_events.pop_from_another_collection_and_add_this_one(globals.events_to_snooze)
         if (event_key_str == None):
             # No more entries to present
             return
 
-def clear_dismissed_events_that_have_ended():
-    global dismissed_events
-
-    dismissed_events.remove_events_based_on_condition(condition_function_for_removing_dismissed_events)
+def clear_dismissed_events_that_have_ended(globals):
+    globals.dismissed_events.remove_events_based_on_condition(condition_function_for_removing_dismissed_events)
 
     return
 
@@ -195,13 +181,10 @@ def condition_function_to_clear_all_events(logger, event_key_str, parsed_event, 
     return(True)
 
 def set_events_to_be_displayed(globals):
-    global dismissed_events
-    global snoozed_events
-
     if (globals.is_reset_needed()):
     # Need to reset the "system" by clearing all of the dismissed and snoozed events
-        dismissed_events.remove_events_based_on_condition(condition_function_to_clear_all_events)
-        snoozed_events.remove_events_based_on_condition(condition_function_to_clear_all_events)
+        globals.dismissed_events.remove_events_based_on_condition(condition_function_to_clear_all_events)
+        globals.snoozed_events.remove_events_based_on_condition(condition_function_to_clear_all_events)
 
         globals.reset_done()
     
@@ -210,7 +193,7 @@ def set_events_to_be_displayed(globals):
         move_events_to_dismiss_into_dismissed_events_collection(globals)
         move_events_to_snooze_into_snoozed_events_collection(globals)
 
-        clear_dismissed_events_that_have_ended()
+        clear_dismissed_events_that_have_ended(globals)
         set_items_to_present_from_snoozed(globals)
     
     for google_account in globals.config.google_accounts:
@@ -223,18 +206,11 @@ def set_events_to_be_displayed(globals):
                 cal_for_account['calendar id'])
 
 def get_events_to_display_main_loop(globals):
-    global dismissed_events
-    global snoozed_events
-
-    dismissed_events = Events_Collection(globals.logger, "dismissed_events", use_rw_lock=True)
-    snoozed_events = Events_Collection(globals.logger, "snoozed_events", use_rw_lock=True)
-
     while True:
         set_events_to_be_displayed(globals)
 
         globals.logger.debug("Going to sleep for " + str(globals.config.refresh_frequency) + " seconds")
         time.sleep(globals.config.refresh_frequency)
-
 
 def start_getting_events_to_display_main_loop_thread(globals):
     main_loop_thread = threading.Thread(
