93a22d3df004916c84aa8146d0e22359b674f76e
Ofir Azoulay-Rozanes <ofiraz@gmail.com>
2024-04-05 22:56:28 -0700

    Improved identification of Meet URLs

--- event_utils.py
+++ event_utils.py
@@ -164,7 +164,7 @@ def has_self_tentative(event):
 video_links_reg_exs = [
     "(https://[a-zA-Z0-9-]*[\.]*zoom\.us/j/[a-zA-Z0-9-_\.&?=/]*)", # Zoom
     "Click here to join the meeting<(https://teams.microsoft.com/l/meetup-join/.*)>", # Teams  
-    "=\"(https://teams.microsoft.com/l/meetup-join/.*)\"", # Teams 
+    "(https://teams.microsoft.com/l/meetup-join/[a-zA-Z0-9-%_./?=]*)", # Teams 
     "[<>](https://[a-zA-Z0-9-]*\.webex\.com/[a-zA-Z0-9-]*/j\.php\?MTID=[a-zA-Z0-9-]*)[<>]", # Webex
     "(https://[a-zA-Z0-9-]*\.webex\.com/meet/[a-zA-Z0-9-\.]*)", # https://rbcteams.webex.com/meet/julian.sequeira
     "(https://chime.aws/[0-9]*)", # AWS Chimes
